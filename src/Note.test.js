27c378a1e84c1d357fbe68bbe16b070472d0d15c
Reuben Coutinho <reuben.coutinho@gmail.com>
2017-07-02 22:03:08 +1000

    test: Updated test cases to import Note.js

--- src/Note.test.js
+++ src/Note.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {shallow} from 'enzyme';
-import Note from './Note';
+import {Note} from './Note';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
